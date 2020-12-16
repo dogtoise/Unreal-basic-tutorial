a592889c8c4d48bb4a7bd73ff2c4db114ca2d796
Lyun-dev <lyun.dev@gmail.com>
2020-12-16 14:23:00 +0900

    this chapter is design of animation system

---
+++ Chapter07_DesignOfAnimationSystem/Intermediate/Build/Win64/UE4Editor/Inc/TutorialProject/ABCharacter.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TUTORIALPROJECT_ABCharacter_generated_h
+#error "ABCharacter.generated.h already included, missing '#pragma once' in ABCharacter.h"
+#endif
+#define TUTORIALPROJECT_ABCharacter_generated_h
+
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_SPARSE_DATA
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_RPC_WRAPPERS
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAABCharacter(); \
+	friend struct Z_Construct_UClass_AABCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AABCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TutorialProject"), NO_API) \
+	DECLARE_SERIALIZER(AABCharacter)
+
+
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAABCharacter(); \
+	friend struct Z_Construct_UClass_AABCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AABCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TutorialProject"), NO_API) \
+	DECLARE_SERIALIZER(AABCharacter)
+
+
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AABCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AABCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AABCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AABCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AABCharacter(AABCharacter&&); \
+	NO_API AABCharacter(const AABCharacter&); \
+public:
+
+
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AABCharacter(AABCharacter&&); \
+	NO_API AABCharacter(const AABCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AABCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AABCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AABCharacter)
+
+
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_PRIVATE_PROPERTY_OFFSET
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_9_PROLOG
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_SPARSE_DATA \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_RPC_WRAPPERS \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_INCLASS \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TutorialProject_Source_TutorialProject_ABCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_SPARSE_DATA \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	TutorialProject_Source_TutorialProject_ABCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TUTORIALPROJECT_API UClass* StaticClass<class AABCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TutorialProject_Source_TutorialProject_ABCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Chapter07_DesignOfAnimationSystem/Intermediate/Build/Win64/UE4Editor/Inc/TutorialProject/ABPlayerController.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TUTORIALPROJECT_ABPlayerController_generated_h
+#error "ABPlayerController.generated.h already included, missing '#pragma once' in ABPlayerController.h"
+#endif
+#define TUTORIALPROJECT_ABPlayerController_generated_h
+
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_SPARSE_DATA
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_RPC_WRAPPERS
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAABPlayerController(); \
+	friend struct Z_Construct_UClass_AABPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AABPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TutorialProject"), NO_API) \
+	DECLARE_SERIALIZER(AABPlayerController)
+
+
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAABPlayerController(); \
+	friend struct Z_Construct_UClass_AABPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AABPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TutorialProject"), NO_API) \
+	DECLARE_SERIALIZER(AABPlayerController)
+
+
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AABPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AABPlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AABPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AABPlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AABPlayerController(AABPlayerController&&); \
+	NO_API AABPlayerController(const AABPlayerController&); \
+public:
+
+
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AABPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AABPlayerController(AABPlayerController&&); \
+	NO_API AABPlayerController(const AABPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AABPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AABPlayerController); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AABPlayerController)
+
+
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_PRIVATE_PROPERTY_OFFSET
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_12_PROLOG
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_PRIVATE_PROPERTY_OFFSET \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_SPARSE_DATA \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_RPC_WRAPPERS \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_INCLASS \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TutorialProject_Source_TutorialProject_ABPlayerController_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_PRIVATE_PROPERTY_OFFSET \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_SPARSE_DATA \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_INCLASS_NO_PURE_DECLS \
+	TutorialProject_Source_TutorialProject_ABPlayerController_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TUTORIALPROJECT_API UClass* StaticClass<class AABPlayerController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TutorialProject_Source_TutorialProject_ABPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
